65246f6046f657ff82f80dc9f74626a0c1d019f2
Michael Grant <serus12345@gmail.com>
2023-04-18 09:42:08 -0400

    second todo

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,50 @@ import React from 'react';
 import axios from 'axios';
 
 
+
 //TODO: Create a state for location, coordinates, and error messages
 
+class Main extends React.Component {
+
+
+constructor(props) {
+    super(props);
+    this.state = {
+        location: '',
+        coordinates: {},
+        errorMessage: '',
+    }
+}
+
 //TODO: getCoordinates() function using axios to get lat and long 
+
+
+getCityData = async (ev) => {
+    ev.preventDefault();
+    try {
+     
+      const url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&q=${this.state.city}&format=json`;
+  
+      
+      const cityDataResponse = await axios.get(url);
+  
+     
+      console.log(cityDataResponse.data[0]);
+       
+      this.setState({
+        cityData: cityDataResponse.data[0],
+        error: false,
+      });
+    } catch (error) {
+      
+      this.setState({
+        error: true,
+        errorMsg: error.message,
+      });
+    }
+  };
+  
+
 
 //TODO: create the LocationInput component that will render an input field and submit button
 // use an event handler to update the location state when input changes
@@ -19,3 +60,6 @@ import axios from 'axios';
 // import the ErrorMessage
 // update the errorMEssage state when an error occurs in the getCoordinates() function
 // render the LocationInput, MapDisplay and ErrorMessage components conditionally based on the coordinates and errorMessage states
+}
+
+export default Main;
